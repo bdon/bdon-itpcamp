1763452fcf3f485edae425fb71e0edc199440c27
Brandon Liu <bdon@bdon.org>
2025-06-13 21:38:50 -0400

    wip

--- create_svg.py
+++ create_svg.py
@@ -6,6 +6,7 @@ from rasterio.warp import transform_bounds, transform_geom
 from rasterio.io import MemoryFile
 from shapely.geometry import shape
 from rasterio.transform import Affine
+from rasterio.enums import Resampling
 import base64
 import xml.etree.ElementTree as ET
 from shapely.affinity import affine_transform
@@ -16,8 +17,8 @@ TIFF_URL = "m_4007317_sw_18_060_20220719.tif"
 
 DPI = 600
 
-x_fudge = 5.5
-y_fudge = 0.5
+x_fudge = 10
+y_fudge = 0.2
 
 wgs_bounds = (-73.991339,40.678184,-73.986517,40.696261)
 
@@ -37,78 +38,97 @@ def rgb_to_grayscale_stretch_np(rgba: np.ndarray) -> np.ndarray:
   gray = ((0.2989 * r + 0.5870 * g + 0.1140 * b) / 4).astype(r.dtype)
   return np.stack([gray, a]) 
 
-with rasterio.open(TIFF_URL) as src:
 
-  # open the source clipping path
-  with fiona.open("prospectpark.geojson", "r") as shap:
-    transformed_geom = transform_geom(
-      src_crs=shap.crs,
-      dst_crs=src.crs,
-      geom=shap[0].geometry
-    )
+raster = rasterio.open(TIFF_URL)
+park_shape = fiona.open("prospectpark.geojson", "r")
 
-    # transform it to the Shapely type
-    poly = shape(transformed_geom)
+# project the shape into the raster CRS
+transformed_geom = shape(transform_geom(
+  src_crs=park_shape.crs,
+  dst_crs=raster.crs,
+  geom=park_shape[0].geometry
+))
 
-    # adjust this to account for the target "right keychain size"
-    FACTOR = 0.3
-    scaling = Affine.scale(FACTOR, FACTOR)
-
-    # get the bounding box of the shape
-    bounds = rasterio.features.bounds(transformed_geom)
-    window = from_bounds(*bounds, transform=src.transform)
-    transform = src.window_transform(window)
-    out_image, out_transform = rasterio.mask.mask(src, [poly], crop=True)
+# get the bounding box of the shape
+bounds = rasterio.features.bounds(transformed_geom)
+window = from_bounds(*bounds, transform=raster.transform)
+print(bounds)
+print(window)
+transform = raster.window_transform(window)
 
 
-    xformed = affine_transform(poly, (~out_transform).to_shapely())
+p = raster.profile.copy()
+p['count'] = 1
 
-    xformed = affine_transform(xformed, scaling.to_shapely())
+tmp_white = MemoryFile().open(**p)
+white_band = np.full(raster.shape, 255, dtype='uint8')
+tmp_white.write(white_band,1)
 
-    xformed = buffer(xformed, 50)
+out_image, out_transform = rasterio.mask.mask(tmp_white, [transformed_geom], crop=True)
+square_inches = np.count_nonzero(out_image) / (600 * 600)
+print("square inches", square_inches)
 
-    # data = src.read(window=window)
+scale_factor = 5 / square_inches
+print("scale factor", scale_factor)
 
-    stretched = rgb_to_grayscale_stretch_np(out_image)
-    with MemoryFile() as memfile:
-      with memfile.open(
-            driver="PNG",
-            width=stretched.shape[2],
-            height=stretched.shape[1],
-            count=stretched.shape[0],
-            dtype=stretched.dtype,
-            crs=src.crs,
-            transform=transform * scaling
-        ) as dst:
-            dst.write(stretched)
-      encoded_image = base64.b64encode(memfile.read()).decode("utf-8")
+# resize the raster to be:
+# 2 square inches at 600 DPI
+print(window)
+data = raster.read(window=window, out_shape = (raster.count, int(window.height * scale_factor), int(window.width * scale_factor)), resampling=Resampling.lanczos)
 
-      ET.register_namespace("xlink", "http://www.w3.org/1999/xlink")
+# it is now the correct size
 
-      svg = ET.Element("svg", {
-          "width": "24in",
-          "height": "12in",
-          "viewBox": "0 0 14400 7200",
-          "version": "1.1",
-          "xmlns": "http://www.w3.org/2000/svg",
-      })
 
-      group = ET.SubElement(svg, "g", {
-        "transform": f"translate({x_fudge * DPI},{y_fudge * DPI})"
-      })
 
-      cut_path = ET.SubElement(group, "path", {
-        "d": linestring_to_svg_path(xformed.exterior),
-        "stroke": "red",
-        "fill": "none",
-        "stroke-width": "0.001 pt"
-      })
+png = MemoryFile() 
+with png.open(driver="PNG",width=data.shape[2],height=data.shape[1],count=4,dtype=np.uint8) as dst:
+  dst.write(data)
 
-      image = ET.SubElement(group, "image", {
-          "width": str(int(stretched.shape[2] * FACTOR)),
-          "height": str(int(stretched.shape[1] * FACTOR)),
-          "{http://www.w3.org/1999/xlink}href": f"data:image/png;base64,{encoded_image}"
-      })
+encoded_image = base64.b64encode(png.read()).decode("utf-8")
 
-      tree = ET.ElementTree(svg)
-      tree.write("output.svg", encoding="UTF-8", xml_declaration=True)
+
+# write the svg
+ET.register_namespace("xlink", "http://www.w3.org/1999/xlink")
+
+svg = ET.Element("svg", {
+    "width": "24in",
+    "height": "12in",
+    "viewBox": "0 0 14400 7200",
+    "version": "1.1",
+    "xmlns": "http://www.w3.org/2000/svg",
+})
+
+group = ET.SubElement(svg, "g", {
+  "transform": f"translate({x_fudge * DPI},{y_fudge * DPI})"
+})
+
+print("park shape")
+print(park_shape)
+xformed = affine_transform(transformed_geom, (~out_transform).to_shapely())
+xformed = affine_transform(xformed, Affine.scale(scale_factor).to_shapely())
+
+cutline = buffer(xformed, 50)
+
+ET.SubElement(group, "path", {
+    "d": linestring_to_svg_path(cutline.exterior),
+    "stroke": "red",
+    "fill": "none",
+    "stroke-width": "0.001pt"
+})
+
+ET.SubElement(group, "path", {
+    "d": linestring_to_svg_path(xformed.exterior),
+    "fill": "black",
+})
+
+ET.SubElement(group, "image", {
+    "width": str(data.shape[2]),
+    "height": str(data.shape[1]),
+    "{http://www.w3.org/1999/xlink}href": f"data:image/png;base64,{encoded_image}"
+})
+
+tree = ET.ElementTree(svg)
+tree.write("output.svg", encoding="UTF-8", xml_declaration=True)
+
+# stretched = rgb_to_grayscale_stretch_np(out_image)
+
